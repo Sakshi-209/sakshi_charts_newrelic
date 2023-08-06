0019a4c9bfe67776df16194749f4b217facdd22d
Sakshi <89094852+Sakshi-209@users.noreply.github.com>
2023-08-06 18:55:28 +0530

    Add files via upload

---
+++ home/areachart.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { AreaChart } from 'nr1'
+
+export default class Areachart extends React.Component{
+    render(){
+        const areachart1 = [
+            {
+                metadata: {
+                  id: 'series-1',
+                  name: 'Serie 1',
+                  color: '#a35ebf',
+                  viz: 'main',
+                  units_data: {
+                    y: 'BYTES',
+                  },
+                },
+                data: [{ y: 128 }],
+              }
+        ]
+        // return <AreaChart data={[areachart1]} fullWidth />;
+        return <AreaChart
+        accountId={4043634}
+        query="SELECT average(host.cpuPercent) AS 'CPU used %' FROM Metric WHERE `entityGuid` = 'NDA0MjczMHxJTkZSQXxOQXwxNDgyOTIzNTg3ODAyOTE4NzU5' TIMESERIES auto" 
+        fullWidth
+      />;
+    }
+}
